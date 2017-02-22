3b5693a4755451c05caba7393222a146d10b726b
Stephen Lazaro <stephen@earnest.com>
2017-02-21 23:12:09 -0800

    Structs and deriving!

--- src/eulers/four.rs
+++ src/eulers/four.rs
@@ -1,3 +1,7 @@
+ // no deriving show :(
+ #[derive(Debug)]
+pub struct ProdPair <a> (a, a);
+
 fn is_palindrome (num: i64) -> bool {
     // Eventually, I'll need this to work
     true
@@ -15,14 +19,14 @@ fn max (x: i64, y: i64) -> i64 {
     if x > y { x } else { y }
 }
 
-pub fn do_ex () -> (i64, i64) {
-    let mut pair = (0, 0);
+pub fn do_ex () -> ProdPair <i64> {
+    let mut pair = ProdPair (0, 0);
     for i in 999..1 {
         // By symmetry, only need those less than i
         for j in i..1 {
             if palindromic_product(i, j) {
                 // return lesser on left
-                pair = (min (j, i), max (j, i));
+                pair = ProdPair (min (j, i), max (j, i));
                 break;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ fn main () {
     println! ("First Euler is {0}", eulers::one::do_ex().to_string());
     println! ("Second Euler is {0}", eulers::two::do_ex().to_string());
     println! ("Third Euler is {0}", eulers::three::do_ex().to_string());
-    println! ("Latest Euler is {0}", eulers::four::do_ex().to_string());
+    println! ("Latest Euler is {:?}", eulers::four::do_ex());
 }
