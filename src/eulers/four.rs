5b06b9e3fee66e97b86c233cb35eaa81c634bf97
Stephen Lazaro <stephen@earnest.com>
2017-02-22 22:56:52 -0800

    Correct if wildly inefficient implementation of problem 4

--- src/eulers/four.rs
+++ src/eulers/four.rs
@@ -3,7 +3,7 @@ use self::num::traits::Num;
 
 // I legit hate this type class derivation syntax...
 # [derive (Debug, PartialEq, Eq)]
-pub struct ProdPair <A : Num> (pub A, pub A);
+pub struct ProdPair <A : Num> (pub A, pub A, pub A);
 
 pub fn is_palindrome <A : Clone + PartialEq> (digits: Vec <A>) -> bool {
     let mut reversed: Vec <A> = digits .clone ();
@@ -38,17 +38,17 @@ fn max (x: i64, y: i64) -> i64 {
 }
 
 pub fn do_ex () -> ProdPair <i64> {
-    let mut pair = ProdPair (0, 0);
-    for i in (100 .. 999) .rev () {
+    let mut pair = ProdPair (0, 0, 0);
+    for i in (100 .. 1000).rev() {
         // By symmetry, only need those less than i
-        for j in (100 .. i) .rev () {
-            if palindromic_product (i, j) {
+        println!("i: {0}", i.to_string());
+        for j in (100 .. 1000).rev() {
+            println!("j: {0}", j.to_string());
+            if palindromic_product (i, j) && i*j > pair.2 {
                 // return lesser on left
-                pair = ProdPair (min (j, i), max (j, i));
-                break;
+                pair = ProdPair (min (j, i), max (j, i), i * j);
             }
         }
-        break;
     }
     pair
 }
